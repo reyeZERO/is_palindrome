5a24130e04413a8b9a6c9bb88e442012c939e92e
reyeZERO <reyescadenaj28@gmail.com>
2025-03-23 23:29:06 -0500

    Create test_palindrome.cpp

---
+++ test_palindrome.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include "palindrome.h"
+
+int main() {
+    assert(is_palindrome("Rotavator"));
+    assert(is_palindrome("x"));
+    assert(is_palindrome("mom"));
+    assert(!is_palindrome("Sam"));
+    assert(!is_palindrome("orange"));
+    assert(!is_palindrome("flower"));
+    return 0;
+}
